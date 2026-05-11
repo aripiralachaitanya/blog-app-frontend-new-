ddba12e7daa7c2b5c484ba4e773c062bde257f01
Aripirala Chaitanya <aripiralachaitanya.new@gmail.com>
2026-05-11 15:37:07 +0530

    fix: update to correct backend URL

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://two4eg110a45-backend-employee.onrender.com"; // or your backend URL
+const BASE_URL = "https://blog-app-backend-2-u741.onrender.com"; // or your backend URL
 
 export default BASE_URL;
